d1148f467ca2e1826474fc274ef43930be28ac00
klserrano <klserranog@ufpso.edu.co>
2026-04-18 11:09:05 -0500

    Se agregan clases de prueba iniciales para el módulo de reservas en TDD

--- src/test/java/com/project/bookingya/tdd/GuestTest.java
+++ src/test/java/com/project/bookingya/tdd/GuestTest.java
@@ -3,30 +3,35 @@ package com.project.bookingya.tdd;
 import com.project.bookingya.dtos.GuestDto;
 import com.project.bookingya.models.Guest;
 import com.project.bookingya.services.GuestService;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.annotation.Order;
 import org.springframework.boot.test.context.SpringBootTest;
+
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class GuestTest {
     @Autowired
-    GuestService guestService;
+    GuestService  guestService;
 
     @Test
     @Order(1)
-    void testCreateGuest() throws Exception {
+    void TestCreateGuest() throws Exception{
         GuestDto guestDto = new GuestDto();
-        guestDto.setIdentification("3323");
+        guestDto.setIdentification("1091677888");
         guestDto.setName("Camila Sarabia");
-        guestDto.setEmail("Prueba@gmail.com");
+        guestDto.setEmail("PruebaQA@gmail.com");
         Guest guest = guestService.create(guestDto);
         assertNotNull(guest);
         assertEquals(guest.getIdentification(), guestDto.getIdentification());
         assertEquals(guest.getName(), guestDto.getName());
         assertEquals(guest.getEmail(), guestDto.getEmail());
     }
-}
+
+    }
