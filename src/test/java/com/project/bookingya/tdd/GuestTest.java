2c801de9ca5ba9534e450ae406591f1be35cd44f
klserrano <klserranog@ufpso.edu.co>
2026-04-18 15:19:09 -0500

    Se agregan clases de prueba iniciales para el módulo de reservas en TDD

--- src/test/java/com/project/bookingya/tdd/GuestTest.java
+++ src/test/java/com/project/bookingya/tdd/GuestTest.java
@@ -1,6 +1,7 @@
 package com.project.bookingya.tdd;
 
 import com.project.bookingya.dtos.GuestDto;
+import com.project.bookingya.exceptions.EntityNotExistsException;
 import com.project.bookingya.models.Guest;
 import com.project.bookingya.services.GuestService;
 import org.junit.jupiter.api.MethodOrderer;
@@ -10,9 +11,7 @@ import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
@@ -22,7 +21,7 @@ public class GuestTest {
 
     @Test
     @Order(1)
-    void TestCreateGuest() throws Exception{
+    void testCreateGuest() throws Exception{
         GuestDto guestDto = new GuestDto();
         guestDto.setIdentification("1091677888");
         guestDto.setName("Camila Sarabia");
@@ -32,6 +31,55 @@ public class GuestTest {
         assertEquals(guest.getIdentification(), guestDto.getIdentification());
         assertEquals(guest.getName(), guestDto.getName());
         assertEquals(guest.getEmail(), guestDto.getEmail());
+    }
+
+    @Test
+    @Order(2)
+    void testGetGuestByIdentification() throws Exception{
+        //Buscamos el huésped directamente por su identificación
+        String identificacionBuscada = "1091677888";
+        Guest guest = guestService.getByIdentification(identificacionBuscada);
+
+        //¡Aquí ponemos tu mensaje de éxito para la consola!
+        System.out.println("¡Búsqueda exitosa! Se encontró al huésped: " + guest.getName() + " con ID: " + guest.getIdentification());
+
+        //Validación final: Comparamos que el ID encontrado sea exactamente el que pedimos
+        assertEquals(identificacionBuscada, guest.getIdentification());
+    }
+
+    @Test
+    @Order(3)
+    void testGetGuestById() throws Exception{
+        String identificacionBuscada = "1091677888";
+        Guest guestPorIdentificacion = guestService.getByIdentification(identificacionBuscada);
+        Guest guestPorId = guestService.getById(guestPorIdentificacion.getId());
+        System.out.println("¡Búsqueda exitosa! Se encontró al huésped por Id: " + guestPorId.getName() + " con ID: " + guestPorId.getId());
+        assertEquals(guestPorIdentificacion.getIdentification(), guestPorId.getIdentification());
+        assertEquals(guestPorIdentificacion.getId(), guestPorId.getId());
+    }
+
+    @Test
+    @Order(4)
+    void testUpdateGuest() throws Exception{
+        String identificacionBuscada = "1091677888";
+        Guest guest = guestService.getByIdentification(identificacionBuscada);
+        GuestDto guestDto = new GuestDto();
+        guestDto.setName("Camilo Sarabia");
+        Guest guestUpdate = guestService.update(guestDto,guest.getId());
+        assertEquals(guestDto.getName(), guestUpdate.getName());
+    }
+
+    @Test
+    @Order(5)
+    void testDeleteGuest() throws Exception{
+        String identificacionBuscada = "3";
+        Guest guest = guestService.getByIdentification(identificacionBuscada);
+        guestService.delete(guest.getId());
+
+        // Intentamos buscarlo después de borrado
+        assertThrows(EntityNotExistsException.class, () -> {
+            guestService.getById(guest.getId());
+        });
     }
 
     }
